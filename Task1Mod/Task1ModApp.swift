c0b3c5a7e832a09213dd40caaa68144fa5e4498b
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2022-11-17 08:45:41 -0500

    Initial Commit

---
+++ Task1Mod/Task1ModApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task1ModApp.swift
+//  Task1Mod
+//
+//  Created by Lucas García on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Task1ModApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
